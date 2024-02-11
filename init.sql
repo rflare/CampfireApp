1ddf4d1db362090169f2d495e70163fadc4b854d
rflare <russellymallari@gmail.com>
2024-02-10 20:30:06 -0500

    Slight database rename, also removed comments

--- init.sql
+++ init.sql
@@ -20,25 +20,11 @@
 --
 
 DROP TABLE IF EXISTS `posts`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `posts` (
     `id` int NOT NULL AUTO_INCREMENT,
     `text` text DEFAULT NULL,
     `name` char(30) DEFAULT NULL,
-    `time_millis` bigint(20) DEFAULT NULL,
+    `timeMillis` bigint(20) DEFAULT NULL,
     PRIMARY KEY (id)
   
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2024-02-01  9:53:58
+)
